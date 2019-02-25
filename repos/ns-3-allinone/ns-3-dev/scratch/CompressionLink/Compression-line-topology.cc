2dc2f2d3d771957e2cfdff78e205acb183ffd264
kedarkhetia <kedarkhetia@gmail.com>
2019-02-25 01:15:10 -0800

    Topology created

---
+++ repos/ns-3-allinone/ns-3-dev/scratch/CompressionLink/Compression-line-topology.cc
@@ -0,0 +1,96 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+ // Network topology
+ //                           Compression link
+ //         8 Mb/s, 2ms      1,2,..10 Mb/s, 2ms      8 Mb/s, 2ms
+ //     n0---------------n1----------------------n2---------------n3
+ //   Server           Router                  Router           Client
+
+ #include <iostream>
+ #include <fstream>
+ #include <string>
+ #include <cassert>
+ 
+ #include "ns3/core-module.h"
+ #include "ns3/network-module.h"
+ #include "ns3/internet-module.h"
+ #include "ns3/point-to-point-module.h"
+ #include "ns3/applications-module.h"
+ #include "ns3/flow-monitor-helper.h"
+ #include "ns3/ipv4-global-routing-helper.h"
+ #include "ns3/queue.h"
+
+using namespace std;
+using namespace ns3;
+ 
+ NS_LOG_COMPONENT_DEFINE ("CompressionLineTopology");
+ 
+ int 
+ main (int argc, char *argv[])
+ {
+
+	 Time::SetResolution (Time::MS);
+	 LogComponentEnable ("UdpServer", LOG_LEVEL_INFO);
+	 LogComponentEnable ("UdpClient", LOG_LEVEL_INFO);
+
+	 NodeContainer nodes;
+	 nodes.Create (4);
+
+	 InternetStackHelper stack;
+	 stack.Install (nodes);
+
+	 PointToPointHelper p0p1;
+	 p0p1.SetDeviceAttribute ("DataRate", StringValue ("8Mbps"));
+	 p0p1.SetChannelAttribute ("Delay", StringValue ("2ms"));
+
+	 PointToPointHelper p1p2;
+	 p1p2.SetDeviceAttribute ("DataRate", StringValue ("8Mbps"));
+	 p1p2.SetChannelAttribute ("Delay", StringValue ("2ms"));
+
+	 PointToPointHelper p2p3;
+	 p2p3.SetDeviceAttribute ("DataRate", StringValue ("8Mbps"));
+	 p2p3.SetChannelAttribute ("Delay", StringValue ("2ms"));
+
+	 Ipv4AddressHelper address;
+	 NetDeviceContainer devices;
+
+	 devices = p0p1.Install (nodes.Get(0), nodes.Get(1));
+	 address.SetBase ("10.1.1.0", "255.255.255.0");
+	 Ipv4InterfaceContainer interface1 = address.Assign (devices);
+
+	 devices = p1p2.Install (nodes.Get(1), nodes.Get(2));
+	 address.SetBase ("10.1.2.0", "255.255.255.0");
+	 Ipv4InterfaceContainer interface2 = address.Assign (devices);
+	 PointerValue ptr;
+
+	 Ptr<PointToPointNetDevice> net0 = DynamicCast<PointToPointNetDevice>(devices.Get(0));
+	 net0->CompressIf(true);
+	 Ptr<PointToPointNetDevice> net1 = DynamicCast<PointToPointNetDevice>(devices.Get(1));
+	 net1->CompressIf(true);
+
+	 devices = p2p3.Install (nodes.Get(2), nodes.Get(3));
+	 address.SetBase ("10.1.3.0", "255.255.255.0");
+	 Ipv4InterfaceContainer interface3 = address.Assign (devices);
+
+	 Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+
+	 uint16_t port = 4000;
+	 UdpServerHelper server (port);
+	 ApplicationContainer apps = server.Install (nodes.Get (3));
+	 apps.Start (Seconds (1.0));
+	 apps.Stop (Seconds (10.0));
+
+	 uint32_t MaxPacketSize = 1024;
+	 Time interPacketInterval = Seconds (0.05);
+	 uint32_t maxPacketCount = 10;
+	 UdpClientHelper client (interface3.GetAddress(1), port);
+	 client.SetAttribute ("MaxPackets", UintegerValue (maxPacketCount));
+	 client.SetAttribute ("Interval", TimeValue (interPacketInterval));
+	 client.SetAttribute ("PacketSize", UintegerValue (MaxPacketSize));
+	 apps = client.Install (nodes.Get (0));
+	 apps.Start (Seconds (2.0));
+	 apps.Stop (Seconds (10.0));
+
+	 Simulator::Run ();
+	 Simulator::Destroy ();
+	 return 0;
+ }
